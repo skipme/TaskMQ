05a826c72260f1a541b0cb54ccd396206a1c4444
cerriun <cerriun@gmail.com>
2014-01-22 15:33:43 +0400

    [add] some build servers mockup

---
+++ SourceControl/buildServers/BuildServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskQueue.Providers;
+
+namespace SourceControl.buildServers
+{
+    public interface BuildServer
+    {
+        public string Name;
+        public string Description;
+
+        public TaskMessage GetParametersModel();
+        public void SetParameters(TaskMessage parameters);
+
+        public byte[] GetArtifacts();
+    }
+}
---
+++ SourceControl/buildServers/TeamCity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SourceControl.buildServers
+{
+    public class TeamCity : BuildServer
+    {
+        // required parameters: build type, artifact name
+        // required connection parameters: host, user, password
+
+        // http://localhost:8380/httpAuth/app/rest/builds?buildType=TaskMQ_DefaltBC&status=SUCCESS
+        // http://localhost:8380/httpAuth/app/rest/builds/id:7
+        // http://localhost:8380/httpAuth/app/rest/builds/id:7/artifacts/children
+        // http://localhost:8380/httpAuth/app/rest/builds/id:7/artifacts/content/debug.zip
+    }
+}
