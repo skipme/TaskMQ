1d1500f208cf91d2f1a319cef30ace1552f303a1
cerriun <cerriun@gmail.com>
2015-08-14 04:48:47 +0300

    [for succesfull build]

--- MSSQLQueue/SqlQuery.cs
+++ MSSQLQueue/SqlQuery.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.SqlClient;
+//using System.Linq;
+//using System.Text;
 
-namespace MSSQLQueue
-{
-    public class SqlQuery
-    {
-        public static object QueryScalar(string sql, string connectionString, params KeyValuePair<string, object>[] parameters)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                SqlCommand command = connection.CreateCommand();
-                command.CommandText = sql;
-                foreach (KeyValuePair<string, object> kv in parameters)
-                {
-                    command.Parameters.AddWithValue(kv.Key, kv.Value);
-                }
-                return command.ExecuteScalar();
-            }
-        }
-    }
-}
+//namespace MSSQLQueue
+//{
+//    public class SqlQuery
+//    {
+//        public static object QueryScalar(string sql, string connectionString, params KeyValuePair<string, object>[] parameters)
+//        {
+//            using (SqlConnection connection = new SqlConnection(connectionString))
+//            {
+//                connection.Open();
+//                SqlCommand command = connection.CreateCommand();
+//                command.CommandText = sql;
+//                foreach (KeyValuePair<string, object> kv in parameters)
+//                {
+//                    command.Parameters.AddWithValue(kv.Key, kv.Value);
+//                }
+//                return command.ExecuteScalar();
+//            }
+//        }
+//    }
+//}
--- updateModulePackage/Program.cs
+++ updateModulePackage/Program.cs
@@ -15,51 +15,53 @@ namespace updateModulePackage
         [STAThread]
         public static void Main(string[] args)
         {
-            string scmUrl = "";// git only now
-            string scmProjectPath = "";
-            string moduleName = "";
+            //string scmUrl = "";// git only now
+            //string scmProjectPath = "";
+            //string moduleName = "";
 
-            if (args[0] == "artifacts")
-            {
-                string rev = Guid.NewGuid().ToString();
-                SourceControl.BuildServers.BuildArtifacts bas = SourceControl.BuildServers.BuildArtifacts.FromDirectory(args[1], rev);
-                AssemblyBinVersions Versions = new AssemblyBinVersions(System.IO.Directory.GetCurrentDirectory(), args[2]);
-                Versions.AddVersion(new SourceControl.Ref.SCMRevision()
-                {
-                    Commiter = "updateModulePackage tool",
-                    CommitTime = DateTime.UtcNow,
-                    CreateAt = DateTime.UtcNow,
-                    Revision = rev
-                }, bas);
-                return;
-            }
-            if (args.Length != 3)
-            {
-                Console.WriteLine("not enough parameters, and make sure it in following sequence: ");
-                Console.WriteLine("scmURL scmRelativeProjectPath moduleName");
-                Console.ReadLine();
-                return;
-            }
-            else
-            {
+            //if (args[0] == "artifacts")
+            //{
+            //    string rev = Guid.NewGuid().ToString();
+            //    SourceControl.BuildServers.BuildArtifacts bas = SourceControl.BuildServers.BuildArtifacts.FromDirectory(args[1], rev);
+            //    AssemblyBinVersions Versions = new AssemblyBinVersions(System.IO.Directory.GetCurrentDirectory(), args[2]);
+            //    Versions.AddVersion(new SourceControl.Ref.SCMRevision()
+            //    {
+            //        Commiter = "updateModulePackage tool",
+            //        CommitTime = DateTime.UtcNow,
+            //        CreateAt = DateTime.UtcNow,
+            //        Revision = rev
+            //    }, bas);
+            //    return;
+            //}
+            //if (args.Length != 3)
+            //{
+            //    Console.WriteLine("not enough parameters, and make sure it in following sequence: ");
+            //    Console.WriteLine("scmURL scmRelativeProjectPath moduleName");
+            //    Console.ReadLine();
+            //    return;
+            //}
+            //else
+            //{
                
-                scmUrl = args[0];
-                scmProjectPath = args[1];
-                moduleName = args[2];
-            }
+            //    scmUrl = args[0];
+            //    scmProjectPath = args[1];
+            //    moduleName = args[2];
+            //}
 
-            SourceControl.BuildServers.NaiveMSfromGit git = new SourceControl.BuildServers.NaiveMSfromGit();
-            git.SetParameters(new SourceControl.BuildServers.NaiveMSfromGitParams()
-            {
-                SCM_URL = scmUrl,
-                ProjectPath = scmProjectPath,
-                AssemblyPath = null
-            });
-            SourceControl.Assemblys.SourceController prj = new SourceControl.Assemblys.SourceController(System.IO.Directory.GetCurrentDirectory(), moduleName, git);
+            //SourceControl.BuildServers.NaiveMSfromGit git = new SourceControl.BuildServers.NaiveMSfromGit();
+            //git.SetParameters(new SourceControl.BuildServers.NaiveMSfromGitParams()
+            //{
+            //    SCM_URL = scmUrl,
+            //    ProjectPath = scmProjectPath,
+            //    AssemblyPath = null
+            //});
+            //SourceControl.Assemblys.SourceController prj = new SourceControl.Assemblys.SourceController(System.IO.Directory.GetCurrentDirectory(), moduleName, git);
 
-            prj.Fetch();
-            prj.Build();
-            prj.UpdatePackage();
+            //prj.Fetch();
+            //prj.Build();
+            //prj.UpdatePackage();
+
+
             //bool sourceUpdateResult = prj.SetUpSourceToDate();
             //if (sourceUpdateResult)
             //{
